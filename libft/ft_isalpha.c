671bfc09bc115fec729d8bc66719b24a1adeb1d8
hi <86646149+habreen@users.noreply.github.com>
2025-11-25 17:11:15 +0000

    update1

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -3,19 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hshamudh <hshamudh@student.42singapore.    +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 11:52:15 by hshamudh          #+#    #+#             */
-/*   Updated: 2025/11/24 11:52:43 by hshamudh         ###   ########.fr       */
+/*   Updated: 2025/11/25 16:15:07 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
- * ft_isalpha - check for alphabetic character
- * Returns non-zero if c is a letter (A-Z or a-z), 0 otherwise.
- */
 int ft_isalpha(int c)
 {
     return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
