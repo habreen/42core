8a3b382c3a3f3ac3841c659f26f478b208364cd3
hi <86646149+habreen@users.noreply.github.com>
2025-12-01 10:42:30 +0000

    update 5.1

---
+++ libft/ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshamudh <hshamudh@student.42singapore.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 11:52:15 by hshamudh          #+#    #+#             */
+/*   Updated: 2025/12/01 10:42:13 by hshamudh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t i;
+	unsigned char *p;
+
+	p = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		p[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
